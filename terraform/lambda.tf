57e18d8ff39f73d76f518cfda4eda331b446d450
khalidHashim1 <khalidhashim1422@gmail.com>
2026-03-05 13:06:02 +0300

    updating IaC

---
+++ terraform/lambda.tf
@@ -0,0 +1,15 @@
+resource "aws_lambda_function" "mylambdaFunction" {
+        function_name = "TextToSpeachFunction"
+        role          = "arn:aws:iam::665832050840:role/service-role/TextToSpeachFunction-role-bos7yg5b"
+        handler       = "lambda_function.lambda_handler"
+        runtime       = "python3.13"
+        publish       = false
+        
+        # Placeholder to satisfy Terraform requirement
+        s3_bucket = "dummy-bucket-terraform-placeholder"
+        s3_key    = "dummy.zip"
+
+        lifecycle {
+            ignore_changes = [filename, s3_bucket, s3_key]
+        }
+}
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -27,5 +27,39 @@ resource "aws_s3_bucket" "khalidAudio" {
   bucket = "khalid-audio-converter1010"
 }
 
+resource "aws_s3_bucket_public_access_block" "khalidAudio" {
+    bucket = aws_s3_bucket.khalidAudio.id
 
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
+
+# texttospeach.khalidhashim.com
+resource "aws_s3_bucket" "khalidStaticSite" {
+  bucket = "texttospeach.khalidhashim.com"
+
+  tags = {
+    awsApplication = "arn:aws:resource-groups:us-east-1:665832050840:group/khalidhashimCV-WebApplication/04ydkgutdg9qdvdnu1jjvbmipe"
+  }
+}
+
+# texttospeach.khalidhashim.com  Versioning 
+resource "aws_s3_bucket_versioning" "khalidStaticSiteVersioning" {
+    bucket = aws_s3_bucket.khalidStaticSite.id
+
+    versioning_configuration {
+      status = "Enabled"
+    }
+}
+# texttospeach.khalidhashim.com  public access
+resource "aws_s3_bucket_public_access_block" "khalidPublicAccess" {
+    bucket = aws_s3_bucket.khalidStaticSite.id
+
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
 
